ed6acbe49f219ec5de5c8d5ad8962bc1a190063a
hollerith <hollerith@gmail.com>
2017-08-03 16:17:27 +0100

    cross platformed

--- joosters.py
+++ joosters.py
@@ -6,7 +6,18 @@ from random import randint
 
 import os, sys, requests, pattern, json, tweepy
 from bs4 import BeautifulSoup
+
+os.chdir('/home/dad/Documents/tattle')
+
 from six.moves.html_parser import HTMLParser
+
+h = HTMLParser() 
+
+from reverend.thomas import Bayes
+ai = Bayes()
+
+from hackernews import HackerNews
+hn = HackerNews()
 
 #build browser object
 br = mechanize.Browser() 
@@ -25,14 +36,6 @@ br.set_debug_responses(False)
 br.set_debug_redirects(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-
-h = HTMLParser() 
-
-os.chdir('/home/dad/Documents/tattle')
-from doxr import *
-
-from hackernews import HackerNews
-hn = HackerNews()
 
 def getPostIds(nick):
     userl = 'https://hacker-news.firebaseio.com/v0/user/%s.json?print=pretty' % nick
@@ -73,18 +76,18 @@ def getTweets(nick, n=500):
         
     return tweets
 
-nick = 'joosters' # hate this guy joosters 
+nick = 'nthcolumn' # hate this guy joosters 
 posts = getPosts(nick, n=500)
 for post in posts[10:]:
-    guesser.train(nick, post)
+    ai.train(nick, post)
 
 nick = 'th3j35t3r' # hate this guy 
 posts = getTweets(nick, n=500)
 for post in posts[10:]:
-    guesser.train(nick, post)
+    ai.train(nick, post)
     
 nick = 'weev' # hate this guy 
 posts = getReddit(nick, n=500)
 for post in posts[10:]:
-    guesser.train(nick, post)
+    ai.train(nick, post)
 
