e39318bbeb2cc358aed48b9d40e5528d3710e9c9
hollerith <hollerith@gmail.com>
2017-08-25 00:11:40 +0100

    update build script

--- deploy_test.sh
+++ deploy_test.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
 # spin up lemonade stand
-apt-get update && apt-get -y upgrade
-apt-get install git python-pip unzip
+# apt-get -y update && apt-get -y upgrade
+apt-get -y install git python-pip unzip
 git clone https://github.com/hollerith/tattle.git
 cd tattle
 pip install -r requirements.txt
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 python app.py &
-ngrok http 80
+./ngrok http -bind-tls=true 80
+
