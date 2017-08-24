3ffbc24769619ccb04706697d41e33c671e89c16
Niall Farrington <hollerith@gmail.com>
2017-08-24 23:11:46 +0100

    Update deploy_test.sh

--- deploy_test.sh
+++ deploy_test.sh
@@ -1,6 +1,10 @@
+# spin up lemonade stand
 apt-get update && apt-get -y upgrade
-apt-get install git python-pip 
+apt-get install git python-pip unzip
 git clone https://github.com/hollerith/tattle.git
 cd tattle
 pip install -r requirements.txt
-python app.py
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+python app.py &
+ngrok http 80
