66b19c28c2fc5ba5d556be80278215e037bb9bbd
hollerith <hollerith@gmail.com>
2017-08-30 01:34:01 +0100

    added paste scraper, test data

--- recon/joosters.py
+++ recon/joosters.py
@@ -36,6 +36,15 @@ br.set_debug_redirects(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
+def getPasteURLs():
+    pastebin = 'http://pastebin.com/archive'
+    latest = requests.get(pastebin).content
+    soup = BeautifulSoup(latest, "lxml")
+    table = soup.select('table.maintable')
+    links = table[0].findAll('a')
+    urls = [link['href'] for link in links if len(link['href'])<10]
+    return urls
+
 def getPostIds(nick):
     userl = 'https://hacker-news.firebaseio.com/v0/user/%s.json?print=pretty' % nick
     r = requests.get(userl)
