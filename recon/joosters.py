1a613a68ad9602f857c45f55158748327bb4c273
hollerith <hollerith@gmail.com>
2017-09-19 18:47:50 +0100

    rand

--- recon/joosters.py
+++ recon/joosters.py
@@ -8,8 +8,8 @@ import numpy as np
 from random import randint
 from pattern.en import sentiment
 from bs4 import BeautifulSoup
-from six.moves.html_parser import HTMLParser
 
+from six.moves.html_parser import HTMLParser
 h = HTMLParser()
 
 from reverend.thomas import Bayes
@@ -17,25 +17,7 @@ ai = Bayes()
 
 from hackernews import HackerNews
 hn = HackerNews()
-
-#build browser object
-br = mechanize.Browser()
-
-# set cookies
-cookies = cookielib.LWPCookieJar()
-br.set_cookiejar(cookies)
-
-# browser settings (used to emulate a browser)
-br.set_handle_equiv(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
-br.set_debug_http(False)
-br.set_debug_responses(False)
-br.set_debug_redirects(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-
+    
 # pastebin
 def getPasteURLs():
     pastebin = 'http://pastebin.com/archive'
@@ -146,6 +128,26 @@ def countfeature(posts, feature):
     print "Average : ", average
     print "Total: ", total
     
+def getFirefox():
+    #build browser object
+    br = mechanize.Browser()
+    
+    # set cookies
+    cookies = cookielib.LWPCookieJar()
+    br.set_cookiejar(cookies)
+    
+    # browser settings (used to emulate a browser)
+    br.set_handle_equiv(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+    br.set_debug_http(False)
+    br.set_debug_responses(False)
+    br.set_debug_redirects(False)
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    
+    return br
 
 with open('./corpora/sense.txt') as file: text = file.read()
 posts = text.replace('\n','').split('.')
