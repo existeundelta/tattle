fe1c83c9ea6a5f63d022ee0b1f96a6fc76ac874a
hollerith <hollerith@gmail.com>
2017-10-14 02:36:40 +0100

    add brute force to bucketfinder

--- recon/bucketfinder.py
+++ recon/bucketfinder.py
@@ -1,5 +1,7 @@
 import requests, re
 from bs4 import BeautifulSoup
+from itertools import permutations
+from string import ascii_lowercase, digits
 
 regex = re.compile("(\.zip|\.pem|\.sql|\.csv|\.xls|\.doc)", re.I)
 
@@ -19,4 +21,20 @@ with open("results.html", 'w') as outfile:
                 print "%s%s" % (full, match.text)
         except:
             print url 
-            
+
+characters = digits + ascii_lowercase + '.-_'
+for x in permutations(characters, 3):
+    url = ''.join(x) 
+    try:
+        full = 'http://%s.s3.amazonaws.com/' % url
+        sample = requests.get(full).content
+        soup = BeautifulSoup(sample, "lxml")
+        matches = soup.find_all('key', text=regex)
+        for match in matches:
+            path = match.text
+            with open("results.html", 'a') as outfile:
+                outfile.write('<a href="%s%s">%s</a>\n' % (full,path,path))
+            print "%s%s" % (full, match.text)
+    except:
+        print url 
+
